a2eaba388cccac8d4441a6ff6805b605f40e5166
davinderkv1 <davinderkv1@rediffmail.com>
2023-10-14 19:04:25 -0700

    Added more tests

--- MySplitterLibrary/Splitter.cs
+++ MySplitterLibrary/Splitter.cs
@@ -1,4 +1,3 @@
-// MySplitterLibrary/Splitter.cs
 using System;
 
 namespace MySplitterLibrary
@@ -8,7 +7,14 @@ namespace MySplitterLibrary
         public decimal SplitAmount(decimal amount, int numberOfPeople)
         {
             if (numberOfPeople <= 0)
+            {
                 throw new ArgumentException("Number of people must be greater than 0.");
+            }
+
+            if (amount < 0)
+            {
+                return 0; // Handle negative amounts by returning 0 per person.
+            }
 
             return amount / numberOfPeople;
         }
--- SplitterTests/SplitterTests.cs
+++ SplitterTests/SplitterTests.cs
@@ -1,5 +1,5 @@
-// SplitterTests/SplitterTests.cs
 using MySplitterLibrary;
+using System;
 using Xunit;
 
 public class MySplitterTests
@@ -48,4 +48,57 @@ public class MySplitterTests
         // Assert
         Assert.Equal(41.15M, result);
     }
+
+    [Fact]
+    public void SplitAmount_NegativePeople_ThrowsArgumentException()
+    {
+        // Arrange
+        var splitter = new Splitter();
+        decimal amount = 100;
+        int numberOfPeople = -2;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => splitter.SplitAmount(amount, numberOfPeople));
+    }
+
+    [Fact]
+    public void SplitAmount_DecimalAmountAndZeroPeople_ThrowsArgumentException()
+    {
+        // Arrange
+        var splitter = new Splitter();
+        decimal amount = 123.45M;
+        int numberOfPeople = 0;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => splitter.SplitAmount(amount, numberOfPeople));
+    }
+    
+    [Fact]
+    public void SplitAmount_NegativeAmountAndPositivePeople_ReturnsZeroPerPerson()
+    {
+        // Arrange
+        var splitter = new Splitter();
+        decimal amount = -100;
+        int numberOfPeople = 5;
+
+        // Act
+        decimal result = splitter.SplitAmount(amount, numberOfPeople);
+
+        // Assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void SplitAmount_DecimalAmountAndNegativePeople_ThrowsArgumentException()
+    {
+        // Arrange
+        var splitter = new Splitter();
+        decimal amount = 123.45M;
+        int numberOfPeople = -2;
+
+        // Act and Assert
+        Assert.Throws<ArgumentException>(() => splitter.SplitAmount(amount, numberOfPeople));
+    }
+
+    // Add more tests as needed to cover your specific scenarios.
 }
